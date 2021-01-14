388ff50c25a2d8be7deca12d8bdc591bf29ce3e4
ErikaD0203 <erikadulay12345678@gmail.com>
2021-01-15 00:40:01 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/imapp_dulay/Splash_screen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.imapp_dulay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
---
+++ app/src/main/java/mcm/edu/ph/imapp_dulay/register_screen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.imapp_dulay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+    }
+}
